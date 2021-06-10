d1ac8d4f27c4792b49139678c84ccd3e160250d9
ChromeCrysis <anderson.bechelli@gmail.com>
2021-06-10 11:36:06 -0300

    Criação do gráfico de Pizza com os dados corretos

--- desafio/core/views.py
+++ desafio/core/views.py
@@ -1,11 +1,20 @@
 from django.shortcuts import render
 from core.models import Dado
 
+import json
+
 # Create your views here.
 def index(request):
     dados = Dado.objects.all().order_by('-date_event')
-    print(dados)
+    pix = Dado.objects.filter(event_type='PIX')
+    pix_times = pix.count()
+    ted = Dado.objects.filter(event_type='TED')
+    ted_times = ted.count()
+    print(pix_times)
+    print(ted_times)
     context = {
+        'pix': json.dumps(pix_times),
+        'ted': json.dumps(ted_times),
         'dados': dados
     }
     return render(request, "index.html", context)
